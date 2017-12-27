3ab8b03c374b18d5ec651f0d2e67e51723be4a30
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2017-12-27 11:07:30 +0530

    added data binding support

--- app/src/main/java/com/mywings/dictionary/RegistrationScreen.kt
+++ app/src/main/java/com/mywings/dictionary/RegistrationScreen.kt
@@ -1,13 +1,12 @@
 package com.mywings.dictionary
 
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_registration_screen.*
+import com.mywings.dictionary.databinding.ActivityRegistrationScreenBinding
 
 class RegistrationScreen : DictionaryCompactActivity() {
+    private var activityRegistration: ActivityRegistrationScreenBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration_screen)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        activityRegistration = setContentLayout(R.layout.activity_registration_screen)
     }
 }
--- app/src/main/java/com/mywings/dictionary/fragments/BaseFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.mywings.dictionary.fragments
-
-import android.support.v4.app.Fragment
-
-
-abstract class BaseFragment : Fragment()
